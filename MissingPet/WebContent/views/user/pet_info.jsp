a3f37b090501547305819b10a22ab383a333dafd
freejia0 <cafelatte220@gmail.com>
2019-08-27 19:34:56 +0900

    주인접속 관련 view추가, 컨트롤러수정

---
+++ MissingPet/WebContent/views/user/pet_info.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<table>
+		<tbody>
+			<tr>
+				<th>사진</th>
+				<td>
+					<font class="a">사진</font>
+				</td>
+			</tr>
+			<tr>
+				<th>회원아이디</th>
+				<td>
+					<font class="a">회원아이디</font>
+				</td>
+			</tr>
+			<tr>
+				<th>전화번호</th>
+				<td>
+					<font class="a">전화번호</font>
+				</td>
+			</tr>
+			<tr>
+				<th>실종장소</th>
+				<td>
+					<font class="a">실종장소</font>
+				</td>
+			</tr>
+			<tr>
+				<th>실종날짜</th>
+				<td>
+					<font class="a">실종날짜</font>
+				</td>
+			</tr>
+			<tr>
+				<th>종류</th>
+				<td>
+					<font class="a">종류</font>
+				</td>
+			</tr>
+			<tr>
+				<th>보상금</th>
+				<td>
+					<font class="a">보상금</font>
+				</td>
+			</tr>
+			<tr>
+				<th>코멘트</th>
+				<td>
+					<font class="a">코멘트</font>
+				</td>
+			</tr>
+		</tbody>
+</table>
--- MissingPet/src/beans/missing/controller/PetController.java
+++ MissingPet/src/beans/missing/controller/PetController.java
@@ -1,5 +1,29 @@
 package beans.missing.controller;
 
-public class PetController {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/pet")
+public class PetController extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		
+		if(action.equals("register")) {//main페이지에서 동물등록 눌렀을때 페이지 이동
+			RequestDispatcher rd = request.getRequestDispatcher("/views/user/register_pet.jsp");
+			rd.forward(request, response);
+			
+		}else if(action.equals("map")) {//register_put.jsp에서 등록버튼 클릭시 이동페이지
+			response.sendRedirect("/views/user/map.jsp");
+		}
+	}
+	
 }
